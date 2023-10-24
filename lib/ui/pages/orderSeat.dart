e1f174206110088605235ab03f1112eb9acc4d75
hafizz7 <muh.hafizz80@gmail.com>
2023-10-24 17:51:45 +0800

    orderSeat

--- lib/ui/pages/orderSeat.dart
+++ lib/ui/pages/orderSeat.dart
@@ -12,39 +12,75 @@ class _OrderSeatState extends State<OrderSeat> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          children: [
-            Container(
-              height: MediaQuery.of(context).size.height * 0.10,
-              width: MediaQuery.of(context).size.width * 0.26,
-              color: Colors.amber,
-            ),
-            Container(
-              height: MediaQuery.of(context).size.height * 0.10,
-              width: MediaQuery.of(context).size.width * 0.26,
-              color: Colors.green,
-            ),
-            Container(
-              height: MediaQuery.of(context).size.height * 0.10,
-              width: MediaQuery.of(context).size.width * 0.26,
-              color: Colors.blue,
-            ),
-            Container(
-              height: MediaQuery.of(context).size.height * 0.10,
-              width: MediaQuery.of(context).size.width * 0.26,
-              color: Colors.cyan,
-            ),
-            Container(
-              height: MediaQuery.of(context).size.height * 0.10,
-              width: MediaQuery.of(context).size.width * 0.26,
-              color: Colors.orange,
-            ),
-            Container(
-              height: MediaQuery.of(context).size.height * 0.10,
-              width: MediaQuery.of(context).size.width * 0.26,
-              color: Colors.yellow,
-            ),
-          ],
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Container(                
+                height: MediaQuery.of(context).size.height * 0.04,
+                width: MediaQuery.of(context).size.width * 1,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Positioned(
+                      left: -8, // Mengatur posisi ikon dari sisi kiri
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.arrow_circle_left_outlined),
+                        iconSize: 30,                        
+                        color: Color(0xFF6558F5),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.center,
+                      child: const Text(
+                        'Select Time & Seat',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'Raleway',
+                          fontWeight: FontWeight.w500,
+                          height: 0,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.10,
+                width: MediaQuery.of(context).size.width * 1,
+                color: Colors.green,
+                decoration: BoxDecoration(
+                border: Border.all(width: 1),
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.yellow,
+                image: const DecorationImage(
+                    image: AssetImage("assets/Vextor 65.png"),
+                    fit: BoxFit.cover),
+              ),
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.10,
+                width: MediaQuery.of(context).size.width * 0.26,
+                color: Colors.blue,
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.10,
+                width: MediaQuery.of(context).size.width * 0.26,
+                color: Colors.cyan,
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.10,
+                width: MediaQuery.of(context).size.width * 0.26,
+                color: Colors.orange,
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.10,
+                width: MediaQuery.of(context).size.width * 0.26,
+                color: Colors.yellow,
+              ),
+            ],
+          ),
         ),
       ),
     );
