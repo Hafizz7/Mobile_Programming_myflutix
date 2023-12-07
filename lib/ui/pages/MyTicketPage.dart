73d92df86fe497f008f148b10592782d2a57aeaf
KevinYaneld <93066929+KevinYaneld@users.noreply.github.com>
2023-12-07 22:32:05 +0800

    Update Tike Saya

--- lib/ui/pages/MyTicketPage.dart
+++ lib/ui/pages/MyTicketPage.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:myflutix/ui/pages/TicketSuccess.dart';
 
 class MyTicketPage extends StatefulWidget {
   @override
@@ -6,59 +9,87 @@ class MyTicketPage extends StatefulWidget {
 }
 
 class _MyTicketPageState extends State<MyTicketPage> {
-  List<Ticket> myTickets = [
-    Ticket(
-      image: 'Avatar.jpg',
-      title: 'Avatar',
-      date: '15 Oktober 2023',
-      time: '19:00',
-      seatNumber: 'A12,A13,A14',
-    ),
-    Ticket(
-      image: 'Avenger.jpg',
-      title: 'Avenger',
-      date: '20 November 2023',
-      time: '15:30',
-      seatNumber: 'B5',
-    ),
-    Ticket(
-      image: 'Archer.jpg',
-      title: 'Archer',
-      date: '20 November 2023',
-      time: '15:30',
-      seatNumber: 'A5',
-    ),
-    Ticket(
-      image: 'Archer.jpg',
-      title: 'Archer',
-      date: '20 November 2023',
-      time: '15:30',
-      seatNumber: 'A5',
-    ),
-    Ticket(
-      image: 'Archer.jpg',
-      title: 'Archer',
-      date: '20 November 2023',
-      time: '15:30',
-      seatNumber: 'A5',
-    ),
-    Ticket(
-      image: 'Archer.jpg',
-      title: 'Archer',
-      date: '20 November 2023',
-      time: '15:30',
-      seatNumber: 'A5',
-    ),
-  ];
+  User? currentUser;
+  List<Ticket> myTickets = [];
 
   List<Ticket> filteredTickets = [];
 
   TextEditingController searchController = TextEditingController();
 
   @override
+  @override
   void initState() {
-    filteredTickets = myTickets;
+    getUserID();
+
+    // Ganti inisialisasi myTickets dengan pemanggilan fungsi getTransactionHistory
+    getTransactionHistory(currentUser?.uid ?? '').then((tickets) {
+      setState(() {
+        myTickets = tickets;
+        filteredTickets = myTickets;
+      });
+    });
+
     super.initState();
+  }
+
+  Future<void> fetchData() async {
+    try {
+      List<Ticket> tickets =
+          await getTransactionHistory(currentUser?.uid ?? '');
+      setState(() {
+        myTickets = tickets;
+        filteredTickets = myTickets;
+      });
+    } catch (error) {
+      print('Error fetching data: $error');
+    }
+  }
+
+  // Function to get the user ID
+  void getUserID() {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      setState(() {
+        currentUser = user;
+      });
+    }
+  }
+
+  // Function to get transaction history based on user ID
+  Future<List<Ticket>> getTransactionHistory(String idAkun) async {
+    List<Ticket> transactionHistory = [];
+
+    try {
+      // Ambil referensi ke koleksi id_transaksi_ticket dalam dokumen dengan id_akun tertentu
+      CollectionReference transactionCollection = FirebaseFirestore.instance
+          .collection('akun')
+          .doc(idAkun)
+          .collection('id_transaksi_ticket');
+
+      // Ambil data dari Firestore
+      QuerySnapshot querySnapshot = await transactionCollection.get();
+
+      // Iterasi melalui dokumen-dokumen dalam koleksi
+      for (QueryDocumentSnapshot doc in querySnapshot.docs) {
+        print('Raw data from Firestore: ${doc.data()}');
+        try {
+          // Ganti pemanggilan fromMap dengan kolom yang sesuai
+          Ticket ticket = Ticket.fromMap(doc.data() as Map<String, dynamic>);
+          print('Ticket created: $ticket');
+          transactionHistory.add(ticket);
+        } catch (e) {
+          print('Error creating Ticket: $e');
+        }
+      }
+
+      print('Transaction history: $transactionHistory');
+      print('ID Akun: $idAkun');
+    } catch (error) {
+      print('Error fetching transaction history: $error');
+      throw error;
+    }
+
+    return transactionHistory;
   }
 
   @override
@@ -69,6 +100,10 @@ class _MyTicketPageState extends State<MyTicketPage> {
       ),
       body: Column(
         children: <Widget>[
+          Container(
+// child: myTickets,
+            child: Text(currentUser?.uid ?? 'Not logged in'),
+          ),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextField(
@@ -77,8 +112,8 @@ class _MyTicketPageState extends State<MyTicketPage> {
                 setState(() {
                   filteredTickets = myTickets
                       .where((ticket) => ticket.title
-                      .toLowerCase()
-                      .contains(value.toLowerCase()))
+                          .toLowerCase()
+                          .contains(value.toLowerCase()))
                       .toList();
                 });
               },
@@ -91,13 +126,28 @@ class _MyTicketPageState extends State<MyTicketPage> {
               ),
             ),
           ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: filteredTickets.length,
-              itemBuilder: (context, index) {
-                return TicketCard(ticket: filteredTickets[index]);
-              },
-            ),
+          FutureBuilder<List<Ticket>>(
+            future: getTransactionHistory(currentUser?.uid ?? ''),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return CircularProgressIndicator(); // Show loading indicator
+              } else if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                return Text('No transaction history found.');
+              } else {
+                // Display the transaction history
+                List<Ticket> transactionHistory = snapshot.data!;
+                return Expanded(
+                  child: ListView.builder(
+                    itemCount: transactionHistory.length,
+                    itemBuilder: (context, index) {
+                      return TicketCard(ticket: transactionHistory[index]);
+                    },
+                  ),
+                );
+              }
+            },
           ),
         ],
       ),
@@ -109,16 +159,32 @@ class Ticket {
   final String image;
   final String title;
   final String date;
+  final String Bioskop;
   final String time;
-  final String seatNumber;
+  final String id_transaksi;
+  final List<dynamic> seatNumber;
 
   Ticket({
     required this.image,
+    required this.Bioskop,
+    required this.id_transaksi,
     required this.title,
     required this.date,
     required this.time,
     required this.seatNumber,
   });
+
+  factory Ticket.fromMap(Map<String, dynamic> map) {
+    return Ticket(
+      Bioskop: map['bioskop'] ?? '',
+      image: map['gambar'] ?? '',
+      title: map['nama_film'] ?? '',
+      id_transaksi: map['id_transaksi_ticket'],
+      date: map['waktu'] ?? '', // Sesuaikan dengan kolom yang ada di Firestore
+      time: map['jam'] ?? '', // Sesuaikan dengan kolom yang ada di Firestore
+      seatNumber: map['kursi'] ?? '',
+    );
+  }
 }
 
 class TicketCard extends StatelessWidget {
@@ -131,33 +197,85 @@ class TicketCard extends StatelessWidget {
     double screenHeight = MediaQuery.of(context).size.height;
     double imageHeight = screenHeight * 0.2;
 
-    return Card(
-      margin: EdgeInsets.all(8.0),
-      color: Color(0xFF6558F5), // Background color for the entire card
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            flex: 1,
-            child: Image.asset(
-              'assets/${ticket.image}',
-              height: imageHeight,
-              fit: BoxFit.contain,
+    return GestureDetector(
+      onTap: () {        
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => MovieTicketSuccessScreen(
+              id_transaksi: ticket.id_transaksi,
+              tanggalSelect: ticket.date,
+              judulMovie: ticket.title,
+              jamSelect: ticket.time,
             ),
           ),
-          Expanded(
-            flex: 2,
-            child: ListTile(
-              title: Text(
-                ticket.title,
-                style: TextStyle(color: Colors.white), // Text color
-              ),
-              subtitle: Text(
-                '${ticket.date} \n${ticket.time}\n${ticket.seatNumber}',
-                style: TextStyle(color: Colors.white), // Text color
+        );
+      },
+      child: Card(
+        margin: EdgeInsets.all(8.0),
+        color: Color(0xFF6558F5), 
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: Image.network(
+                ticket.image,
+                height: imageHeight,
+                fit: BoxFit.contain,
               ),
             ),
-          ),
-        ],
+            Expanded(
+              flex: 2,
+              child: ListTile(
+                title: Text(
+                  ticket.title,
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                ),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 2,
+                    ),
+                    Text(
+                      ticket.date,
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                    SizedBox(
+                      height: 2,
+                    ),
+                    Text(
+                      ticket.Bioskop, // Tambahkan informasi Bioskop di sini
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                    SizedBox(
+                      height: 2,
+                    ),
+                    Text(
+                      ticket.seatNumber.join(', '),
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  ],
+                ),
+                // trailing: ElevatedButton(
+                //   onPressed: () {
+                //     Navigator.push(
+                //       context,
+                //       MaterialPageRoute(
+                //         builder: (context) => MovieTicketSuccessScreen(
+                //           id_transaksi: ticket.id_transaksi,
+                //           tanggalSelect: ticket.date,
+                //           jamSelect: ticket.time,
+                //         ),
+                //       ),
+                //     );
+                //   },
+                //   child: Text('Details'),
+                // ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
